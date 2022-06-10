e08eae9496bf3a0dd3e12614b9434a1308d5e275
Chris C <post345@outlook.com>
2022-06-11 01:29:05 +0300

    Edit terraform files and notes

---
+++ 01-terraform-basics/resources.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-chris345-001"
+    versioning {
+        enabled = true
+    }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+	name = "my_iam_user_abc_updated"
+}
