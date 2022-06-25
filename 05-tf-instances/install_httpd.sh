173413fc33f8f59d2cd7f749c42faf470180118d
Chris C <post345@outlook.com>
2022-06-25 13:47:38 +0200

    Implement retreive AWS EC2 meta-data (public_dns)

--- 05-tf-instances/install_httpd.sh
+++ 05-tf-instances/install_httpd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install httpd -y 
 sudo service httpd start
-# DNS=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
-sudo echo "Virtual Server link: cc03" > /var/www/html/index.html 
+DNS=$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)
+sudo echo "Virtual Server link: $DNS" > /var/www/html/index.html 
