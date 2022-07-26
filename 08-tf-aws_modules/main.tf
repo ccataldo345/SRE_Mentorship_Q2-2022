e3051737b5c4a69eb66382d6f417035bf7b7e412
Chris C <post345@outlook.com>
2022-07-26 20:55:22 +0300

    Edit tf proj. 08 aws-modules

--- 08-tf-aws_modules/main.tf
+++ 08-tf-aws_modules/main.tf
@@ -2,12 +2,12 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  name = "vpc-modules"
+  cidr = "10.0.0.0/18"
 
-  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.0.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
